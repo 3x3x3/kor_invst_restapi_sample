dcfb67adefeebae2e41a58e07ccd73541ebc7dca
3x3x3 <ivmivm001@gmail.com>
2022-04-07 11:36:53 +0900

    AppKey, AppSecret값을 common 모듈로 얻어온다

--- get_cur_prc.py
+++ get_cur_prc.py
@@ -1,20 +1,10 @@
-import configparser
 import common
 import requests
 
 
-def main():
-    shtcode = '005930'
-
-    cp = configparser.ConfigParser()
-    cp.read('config.ini')
-    app_key = cp['Key']['AppKey']
-    app_secret = cp['Key']['AppSecret']
-
+def main(shtcode: str) -> None:
+    app_key, app_secret = common.get_keys('config.ini')
     token_type, acc_token = common.get_acc_token(app_key, app_secret)
-
-    base_url = 'https://openapi.koreainvestment.com:9443'
-    path = '/uapi/domestic-stock/v1/quotations/inquire-price'
 
     req_header = {
         'content-type': 'application/json; charset=utf-8',
@@ -24,11 +14,12 @@ def main():
         'tr_id': 'FHKST01010100',
     }
 
-    req_url = f'{base_url}/{path}?FID_COND_MRKT_DIV_CODE=J&FID_INPUT_ISCD={shtcode}'
+    path = '/uapi/domestic-stock/v1/quotations/inquire-price'
+    req_url = f'{common.BASE_URL}/{path}?FID_COND_MRKT_DIV_CODE=J&FID_INPUT_ISCD={shtcode}'
 
     resp = requests.get(req_url, headers=req_header)
     print(resp.text)
 
 
 if '__main__' == __name__:
-    main()
+    main('005930')
